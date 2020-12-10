bcfb8265aa01681ec99105df4e308447db11b7ce
AYDEN HOLGATE <AYDEN.HOLGATE@connect.7oaks.org>
2020-12-10 15:30:37 -0600

    Initial commit

---
+++ program7/program7.cpp
@@ -0,0 +1,38 @@
+// program7.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	srand (time(0));
+
+	int num = 0;
+
+	// Output the first line
+
+	cout << "The number generated is ";
+
+	for (int i = 0; i < 100; i++){
+		num = rand() % 50 + 1;								// Generate random numbers
+
+		// you write code to test for odd nunmber
+
+		cout << " " << num;
+
+		if (i % 10 == 0){
+			cout << endl;									// Skip 10 lines
+
+		}
+
+	}
+
+	cout << endl;											// Skip a line
+
+	return 0;
+}
+
